0cd0996e7facf770a4bdb3f094f35ce6e45acff6
Justin Lee <justin.lee@ubc.ca>
2021-05-04 10:00:39 -0700

    Changed folder structure of markdown to mirror output

--- convertMD.js
+++ convertMD.js
@@ -4,21 +4,18 @@ const path = require('path')
 
 const converter = new showdown.Converter()
 
-const viewsPath = ('./views')
-const views = fs.readdirSync(viewsPath)
+const views = fs.readdirSync(path.join('markdown', 'views'))
 
-const staticPath = ('./static')
-
-const index = fs.readFileSync('./index.md', 'utf8')
+const index = fs.readFileSync(path.join('markdown', './index.md'), 'utf8')
 const html = converter.makeHtml(index)
-fs.writeFileSync(path.join(staticPath, 'index.html'), html)
+fs.writeFileSync(path.join('static', 'index.html'), html)
 
 views.forEach(view => {
-  const mdPath = path.join(viewsPath, view)
+  const mdPath = path.join('markdown', 'views', view)
   const md = fs.readFileSync(mdPath, 'utf8')
   const html = converter.makeHtml(md)
 
-  const outputPath = path.join(staticPath, 'views', view.split('.')[0] + '.html')
+  const outputPath = path.join('static', 'views', view.split('.')[0] + '.html')
   fs.writeFileSync(outputPath, html)
 })
 
