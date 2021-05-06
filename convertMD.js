7393cfbe46ba39cfc3bcc995c43b9cff5994f4af
Justin Lee <justin.lee@ubc.ca>
2021-05-06 10:36:05 -0700

    Insert html into template

--- convertMD.js
+++ convertMD.js
@@ -4,16 +4,24 @@ const path = require('path')
 
 const converter = new showdown.Converter()
 
-const views = fs.readdirSync(path.join('markdown', 'views'))
+const template = fs.readFileSync(path.join('_assets', 'template', 'index.html'), 'utf8')
+
+const insertIntoTemplate = (content, template) => {
+  const attachmentPoint = 'id="content">'
+  const [before, after] = template.split(attachmentPoint)
+  return [before, attachmentPoint, content, after].join('')
+}
 
 const index = fs.readFileSync(path.join('markdown', './index.md'), 'utf8')
-const html = converter.makeHtml(index)
+const html = insertIntoTemplate(converter.makeHtml(index), template)
 fs.writeFileSync(path.join('static', 'index.html'), html)
+
+const views = fs.readdirSync(path.join('markdown', 'views'))
 
 views.forEach(view => {
   const mdPath = path.join('markdown', 'views', view)
   const md = fs.readFileSync(mdPath, 'utf8')
-  const html = converter.makeHtml(md)
+  const html = insertIntoTemplate(converter.makeHtml(md), template)
 
   const outputPath = path.join('static', 'views', view.split('.')[0] + '.html')
   fs.writeFileSync(outputPath, html)
