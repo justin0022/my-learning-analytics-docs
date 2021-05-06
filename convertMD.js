005ab02b46497824e0815db83728b85d1fed7575
Justin Lee <justin.lee@ubc.ca>
2021-05-06 10:51:05 -0700

    Removed static files, linked pages

--- convertMD.js
+++ convertMD.js
@@ -1,6 +1,15 @@
 const showdown = require('showdown')
 const fs = require('fs')
 const path = require('path')
+const ncp = require('ncp').ncp
+
+ncp.limit = 16
+
+// copy images into /static
+ncp(path.join('_assets', 'images'), path.join('static', '_assets'), err => err ? console.log(err) : null)
+
+// copy css into /static
+ncp(path.join('_assets', 'template', 'css'), path.join('static', 'css'), err => err ? console.log(err) : null)
 
 const converter = new showdown.Converter()
 
@@ -16,14 +25,14 @@ const index = fs.readFileSync(path.join('markdown', './index.md'), 'utf8')
 const html = insertIntoTemplate(converter.makeHtml(index), template)
 fs.writeFileSync(path.join('static', 'index.html'), html)
 
-const views = fs.readdirSync(path.join('markdown', 'views'))
+const views = fs.readdirSync(path.join('markdown')).filter(x => x !== 'index.md')
 
 views.forEach(view => {
-  const mdPath = path.join('markdown', 'views', view)
+  const mdPath = path.join('markdown', view)
   const md = fs.readFileSync(mdPath, 'utf8')
   const html = insertIntoTemplate(converter.makeHtml(md), template)
 
-  const outputPath = path.join('static', 'views', view.split('.')[0] + '.html')
+  const outputPath = path.join('static', view.split('.')[0] + '.html')
   fs.writeFileSync(outputPath, html)
 })
 
